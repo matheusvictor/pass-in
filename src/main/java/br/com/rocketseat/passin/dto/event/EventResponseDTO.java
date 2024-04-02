af5041a077923aa423bfbee5de31c976d57cbd84
Matheus Victor <matheusvictor.salles@gmail.com>
2024-04-02 13:59:59 -0300

    feat: create event response
    This class will get all data from record DTO
    plus number of attendees, then it'll create a response

---
+++ src/main/java/br/com/rocketseat/passin/dto/event/EventResponseDTO.java
@@ -0,0 +1,20 @@
+package br.com.rocketseat.passin.dto.event;
+
+import br.com.rocketseat.passin.domain.event.Event;
+
+public class EventResponseDTO {
+
+    EventDetailsDTO event;
+
+    public EventResponseDTO(Event event, Integer numberOfAttendees) {
+        this.event = new EventDetailsDTO(
+                event.getId(),
+                event.getTitle(),
+                event.getDetails(),
+                event.getSlug(),
+                event.getMaximumAttendees(),
+                numberOfAttendees
+        );
+    }
+
+}
