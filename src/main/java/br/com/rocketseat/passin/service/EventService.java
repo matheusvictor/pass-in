112d09f2295f0563656315d02ac13b8449d660ab
Matheus Victor <matheusvictor.salles@gmail.com>
2024-04-02 15:40:16 -0300

    feat: add method to create a new event

--- src/main/java/br/com/rocketseat/passin/service/EventService.java
+++ src/main/java/br/com/rocketseat/passin/service/EventService.java
@@ -1,10 +1,15 @@
 package br.com.rocketseat.passin.service;
 
+import br.com.rocketseat.passin.domain.event.Event;
+import br.com.rocketseat.passin.dto.event.EventIdDTO;
+import br.com.rocketseat.passin.dto.event.EventRequestDTO;
 import br.com.rocketseat.passin.dto.event.EventResponseDTO;
 import br.com.rocketseat.passin.repository.AttendeeRepository;
 import br.com.rocketseat.passin.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.text.Normalizer;
 
 
 @Service
@@ -25,4 +30,26 @@ public class EventService {
         return new EventResponseDTO(event, attendeeList.size());
     }
 
+    public EventIdDTO createEvent(EventRequestDTO eventDTO) {
+        var newEvent = new Event();
+
+        newEvent.setTitle(eventDTO.title());
+        newEvent.setDetails(eventDTO.details());
+        newEvent.setMaximumAttendees(eventDTO.maximumAttendees());
+        newEvent.setSlug(generateSlug(eventDTO.title()));
+
+        eventRepository.save(newEvent);
+
+        return new EventIdDTO(newEvent.getId());
+    }
+
+    private String generateSlug(String text) {
+        var normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
+
+        return normalized
+                .replaceAll("\\p{InCOMBINING_DIACRITICAL_MARKS}", "")
+                .replaceAll("[^\\w\\s]", "")
+                .replaceAll("\\s+", "-")
+                .toLowerCase();
+    }
 }
