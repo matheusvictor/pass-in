02d6f763aab24b97c9345a5dc95ab2d326b43072
Matheus Victor <matheusvictor.salles@gmail.com>
2024-04-02 14:09:14 -0300

    feat: add method signature to find all attendees by eventId

--- src/main/java/br/com/rocketseat/passin/repository/AttendeeRepository.java
+++ src/main/java/br/com/rocketseat/passin/repository/AttendeeRepository.java
@@ -4,6 +4,9 @@ import br.com.rocketseat.passin.domain.attendees.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+    List<Attendee> findAllByEventId(String eventId);
 }
