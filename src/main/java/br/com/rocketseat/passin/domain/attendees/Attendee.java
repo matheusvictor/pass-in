6a5b199470ffe70d33ded6e0dbe137ad750a281b
Matheus Victor <matheusvictor.salles@gmail.com>
2024-04-02 11:31:14 -0300

    feat: create domain classes
    These classes will represent our db table too

---
+++ src/main/java/br/com/rocketseat/passin/domain/attendees/Attendee.java
@@ -0,0 +1,36 @@
+package br.com.rocketseat.passin.domain.attendees;
+
+import br.com.rocketseat.passin.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "attendees")
+public class Attendee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/br/com/rocketseat/passin/domain/checkin/CheckIn.java
@@ -0,0 +1,30 @@
+package br.com.rocketseat.passin.domain.checkin;
+
+import br.com.rocketseat.passin.domain.attendees.Attendee;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "check_ins")
+public class CheckIn {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "attendee_id", nullable = false)
+    private Attendee attendee;
+}
