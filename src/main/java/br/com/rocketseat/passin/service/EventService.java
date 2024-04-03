63092ee232e55d0d1b10a6547929590bebaa78c4
Matheus Victor <matheusvictor.salles@gmail.com>
2024-04-03 16:20:27 -0300

    feat: create method to register an attendee to event

--- src/main/java/br/com/rocketseat/passin/service/EventService.java
+++ src/main/java/br/com/rocketseat/passin/service/EventService.java
@@ -1,7 +1,11 @@
 package br.com.rocketseat.passin.service;
 
+import br.com.rocketseat.passin.domain.attendees.Attendee;
 import br.com.rocketseat.passin.domain.event.Event;
+import br.com.rocketseat.passin.domain.event.exception.EventFullException;
 import br.com.rocketseat.passin.domain.event.exception.EventNotFoundException;
+import br.com.rocketseat.passin.dto.attendee.AttendeeIdDTO;
+import br.com.rocketseat.passin.dto.attendee.AttendeeRequestDTO;
 import br.com.rocketseat.passin.dto.event.EventIdDTO;
 import br.com.rocketseat.passin.dto.event.EventRequestDTO;
 import br.com.rocketseat.passin.dto.event.EventResponseDTO;
@@ -10,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.text.Normalizer;
+import java.time.LocalDateTime;
 
 
 @Service
@@ -50,5 +55,28 @@ public class EventService {
                 .replaceAll("[^\\w\\s]", "")
                 .replaceAll("\\s+", "-")
                 .toLowerCase();
+    }
+
+    public AttendeeIdDTO registerAttendee(String eventId, AttendeeRequestDTO attendeeDTO) {
+        attendeeService.verifyAttendeeSubscription(attendeeDTO.email(), eventId);
+
+        var event = eventRepository
+                .findById(eventId)
+                .orElseThrow(() -> new EventNotFoundException(String.format("Event with ID %s not found", eventId)));
+
+        var attendeeList = attendeeService.getAllAttendeesFromEvent(eventId);
+
+        if (event.getMaximumAttendees() <= attendeeList.size()) {
+            throw new EventFullException("Event is full");
+        }
+
+        var newAttendee = new Attendee();
+        newAttendee.setName(attendeeDTO.name());
+        newAttendee.setEmail(attendeeDTO.email());
+        newAttendee.setEvent(event);
+        newAttendee.setCreatedAt(LocalDateTime.now());
+
+        attendeeService.registerAttendee(newAttendee);
+        return new AttendeeIdDTO(newAttendee.getId());
     }
 }
