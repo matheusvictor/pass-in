b82e5f54c74c8eed1f0c437a67e3cbe24e72e5b1
Matheus Victor <matheusvictor.salles@gmail.com>
2024-04-03 08:34:21 -0300

    feat: add method signature to find check-ins by attends ID

--- src/main/java/br/com/rocketseat/passin/repository/CheckInRepository.java
+++ src/main/java/br/com/rocketseat/passin/repository/CheckInRepository.java
@@ -4,6 +4,9 @@ import br.com.rocketseat.passin.domain.checkin.CheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CheckInRepository extends JpaRepository<CheckIn, Integer> {
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
